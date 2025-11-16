58f6f357d777379b24b4c8180246b7ce2e9e8b3f
ethan golledge <ethg@hotmail.co.uk>
2025-11-16 09:49:38 +0000

    Slightly better error handling. Need to add some logging so the user can check the logs if there are errors

--- googledrive/downloader.py
+++ googledrive/downloader.py
@@ -18,15 +18,17 @@ def format_list_with_and(items: list[str]) -> str:
         return f"{items[0]} and {items[1]}"
     return ", ".join(items[:-1]) + ", and " + items[-1]
 
-def orchestrate_download():
-    FOLDER_ID = '1hTNH5woIRio578onLGElkTWofUSWRoH_'
-    
-    gdrive_instance = GoogleDrive(folder_id=FOLDER_ID)
+def orchestrate_download(folder_id: str, api_key: str = None, download_dir: str = None) -> (pl.DataFrame, GoogleDrive):
+    """due to the nature of extreme variability in document file types, we will ask the user what is to be downloaded and warn if there is support for the stated file types"""
+    gdrive_instance = GoogleDrive(
+        folder_id=folder_id,
+        api_key=api_key,
+        download_dir=download_dir)
     all_files_df = gdrive_instance.scan_all_files()
 
     if all_files_df.is_empty():
         print("No files found in the Google Drive folder.")
-        return
+        return None, None
 
     supported_config = ProcessableFileTypes()
     files_to_download_df = None
@@ -38,7 +40,7 @@ def orchestrate_download():
 
         if user_choice == 'none':
             print('No downloads will occur.')
-            return
+            return None, None
         
         elif user_choice == 'all':
             print('Commencing download of all file types...')
@@ -75,38 +77,69 @@ def orchestrate_download():
 
     if files_to_download_df is None or files_to_download_df.is_empty():
         print("No files found matching your criteria.")
-        return
+        return None, None
 
     print(f"\nFound {len(files_to_download_df)} files to download. Starting process...")
+
+    return files_to_download_df, gdrive_instance
    
-    
+def download_files(df: pl.DataFrame, gdrive_instance: GoogleDrive) -> pl.DataFrame:
+    """download files in a loop, passing contects of the dataframe as a dictionary with each iteration"""
     download_results = []
 
-    for i, row in enumerate(files_to_download_df.iter_rows(named=True)):
-        if i == 3:
+    for i, row in enumerate(df.iter_rows(named=True)):
+        if i == 100:
             break
     
         status = gdrive_instance.download_file(row)
         print(status)
 
-        err_flag = ("Error" in status or "Skipped" in status)
+        err_flag = ('Error' in status or 'Skipped' in status)
 
         download_results.append({
-            "id": row["id"],
-            "error": err_flag
+            'id': row['id'],
+            'error': err_flag
         })
 
     down_results_df = pl.DataFrame(
     data=download_results,
     schema={
-        "id": pl.Utf8,
-        "error": pl.Boolean
+        'id': pl.Utf8,
+        'error': pl.Boolean
     })
 
-    final_df = files_to_download_df.join(down_results_df, on="id", how="left")
+    final_df = df.join(down_results_df, on="id", how="left")
 
-    total_errs = final_df["error"].sum()
-    print(f"\nDownload process finished. Total Errors: {total_errs}")
+    return final_df
+
+def main(folder_id: str, api_key: str = None, download_dir: str = None):
+    filtered_df, gdrive_instance = orchestrate_download(
+        folder_id=folder_id,
+        api_key=api_key,
+        download_dir=download_dir)
+
+    if filtered_df is None:
+        return
+
+    download_results_df = download_files(df=filtered_df, gdrive_instance=gdrive_instance)
+    total_errs = download_results_df['error'].sum()
+    print(f'\nDownload process finished. Total Errors: {total_errs}')
+    if total_errs > 0:
+        print("Shall we proceed with a retry of the rows with errors? (y/n):")
+        resp = input()
+        if resp.lower().strip() == 'y':
+            errs_df = download_results_df.filter(pl.col('error') == True)
+            retry_results_df = download_files(df=errs_df, gdrive_instance=gdrive_instance)
+            
+            final_results_df = download_results_df.update(retry_results_df, on='id')
+            
+            final_errs = final_results_df['error'].sum()
+            print(f"Second download complete. Errors: {final_errs}. Please investigate further.")
+        else:
+            return
+    else:
+        return
 
 if __name__ == "__main__":
-    orchestrate_download()
+    FOLDER_ID = '1hTNH5woIRio578onLGElkTWofUSWRoH_'
+    main(folder_id=FOLDER_ID)
